f5d806aeb55cc3bd120487435090634f6758a894
TahmidJamal <tjamal@stuy.edu>
2018-04-12 00:45:40 -0400

    HW96 v0, solver for base cases

--- MazeSolver.java
+++ MazeSolver.java
@@ -5,5 +5,18 @@
   dropped on an arbitrary start position to legally trace a 
   path to the treasure.
  */
- 
- 
+public class MazeSolver {
+    
+    private Maze maze;
+    public MazeSolver( Maze maze) {
+        this.maze = maze;
+    }
+    public boolean solve() {
+        if (maze.explorerIsOnA() == maze.TREASURE) {
+            return true;
+        } else if (maze.explorerIsOnA() == maze.WALL) {
+            return false;
+        }
+        return false;
+    }
+}
--- UserOfMazeSolver.java
+++ UserOfMazeSolver.java
@@ -4,24 +4,25 @@
   Requires command line arguments:
   o  the name of a file containing a maze.
   o  the rank and file where an explorer is starting
-
-  For example,
-      java UserOfMaze mazes/4cell_treasureWest.txt -1 -1 
-      
+  
  */
 public class UserOfMazeSolver {
     private static Displayer displayer;
     
-    public static void main(String[] args) {
-        //following up until next comment is taken from UserOfMaze.java
+    public static void main(String[] args) 
+        
         throws java.io.FileNotFoundException {
         System.out.println();
 
         Maze maze = new Maze( commandLine[0], Integer.parseInt( commandLine[1]), Integer.parseInt( commandLine[2]));
         System.out.println( maze + System.lineSeparator());
-        //
-        
+      
+        Displayer displayer = new Displayer( Integer.parseInt( commandLine[3]));
+
+        MazeSolver solver = new MazeSolver( maze);
+        System.out.println(solver.solve());
+
         }
-    }
+    
     
 }
