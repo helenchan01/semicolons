8fbf27060caab3f699ed0b36624d60fb55282d53
helenchan01 <34070758+helenchan01@users.noreply.github.com>
2018-04-12 23:29:02 -0400

    v0

--- MazeSolver_v0.java
+++ MazeSolver_v0.java
@@ -7,10 +7,12 @@
  */
 public class MazeSolver_v0 {
     
+    private static final int[] directions = { Maze.NORTH, Maze.EAST, Maze.SOUTH, Maze.WEST );
     private Maze maze;
     public MazeSolver( Maze maze) {
         this.maze = maze;
     }
+	
     public boolean solve() {
         if (maze.explorerIsOnA() == maze.TREASURE) {
             return true;
@@ -18,13 +20,25 @@ public class MazeSolver_v0 {
         if (maze.explorerIsOnA() == maze.WALL) {
             return false;
         }
-	Maze snapshot = new Maze(maze);
-	maze.go(Maze.EAST);
-	if (maze.explorerIsOnA() == maze.WALL) {
-		
-	//dummy.go(Maze.EAST);
-	//MazeSolver mini = new MazeSolver ( dummy);
-	//return mini.solve();
+	// for every direction
+	for (int dirIndex = 0; dirIndex < 4; dirIndex++ ) {
+	    Maze snapshot = new Maze(maze);
+	    // debugging
+	    System.out.println("snapshot" + System.lineSeparator() + snapshot);	
+	    maze.dropA(Maze.WALL);		
+	    maze.go(directions[dirIndex]);
+	    // if the explorer in recursive maze reaches the treasure, there is a solution	
+            if (maze.solve()) {
+		return true;
+	    }
+	    // if explorer hits a wall, restore to previous version	
+	    else {
+		// debugging
+		System.out.println("before restoring" + System.lineSeparator() + maze);    
+		maze = snapshot;
+		System.out.println("after restoring" + System.lineSeparator() + maze);  
+	    }
+	}
 	return false;
     }
 }
